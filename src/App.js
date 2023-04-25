b7ec457247a46ddc4120818c372170fac8e677bf
Imandos07 <aliserovimandos07@gmail.com>
2023-04-26 00:21:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+function Square(props) {
+  const [color, setColor] = useState(props.color);
+
+  function handleClick() {
+    setColor(color === "red" ? "blue" : "red");
+  }
+
+  return (
+    <div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: color,
+        margin: "10px",
+      }}
+      onClick={handleClick}
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Square color="red" />
+      <Square color="blue" />
     </div>
   );
 }
 
 export default App;
+
